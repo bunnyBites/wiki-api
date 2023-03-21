f29e6b381b4218ada6defe149944a1e61186560f
bunnyBites <s.mukeshkr481@gmail.com>
2023-03-21 22:43:18 +0530

    1. add CRUD All methods 2. add CRUD methods specific to title

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = (process.env.PORT || 3002);
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello</h1>")
+// connect with mongo
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB").then(() => console.log("Connected with mongoDB!"))
+
+const ArticleSchema = new mongoose.Schema({ title: String, content: String });
+const Article = new mongoose.model("Article", ArticleSchema);
+
+app.route("/articles")
+.get((req, res) => {
+  Article.find({})
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
 })
+.post((req, res) => {
+  const { title, content } = req.body;
+
+  Article.create({ title, content})
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
+})
+.delete((req, res) => {
+  Article.deleteMany({})
+    .then(() => res.send("Delete successfull!"))
+    .catch((err) => res.send(err));
+});
+
+app.route("/articles/:title")
+.get((req, res) => {
+  const { title } = req.params;
+  Article.find({ title })
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
+})
+.put((req, res) => {
+  const { title, content } = req.query;
+  console.log(title, content);
+  Article.updateOne({ title: req.params.title }, { title, content })
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err))
+})
+.delete((req, res) => {
+  const { title } = req.params;
+
+  Article.deleteOne({ title })
+    .then(() => res.send("Delete Successfull"))
+    .catch((err) => res.send(err));
+});
 
 app.listen(port, () => console.log("Server is running on port " + port));
